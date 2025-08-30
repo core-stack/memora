1178ed8684b3e91f9929d84b6455042a6e6687ee
mayron1806 <mayronfernandes01@gmail.com>
2025-08-30 09:10:38 -0300

    chore: add prisma and basic entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from "./app.service";
 import { IngestModule } from "./modules/ingest/ingest.module";
 import { SearchModule } from "./modules/search/search.module";
 import { DatabaseModule } from "./services/database/database.module";
+import { PrismaModule } from './services/prisma/prisma.module';
 
 @Module({
   imports: [
     IngestModule,
     SearchModule,
-    DatabaseModule
+    DatabaseModule,
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/services/prisma/prisma.service.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from 'src/@generated/prisma';
+
+import { Injectable, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
