189e78cecf5338cb54ba332db4f0cfd28ac646fb
Mayron <mayron.fernandes@alghorit.com.br>
2025-09-04 13:31:28 -0300

    build: add api url env

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,23 +1,25 @@
-import path from 'path';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+    server: {
+      proxy: {
+        "/api": {
+          target: env.API_URL ?? "http://localhost:3000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        }
       }
     }
   }
